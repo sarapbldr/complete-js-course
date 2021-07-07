8f4b8cc4a15b439a16d8d380bcfc9205b2343b71
Eric Sández <esandez93@gmail.com>
2021-07-07 20:27:11 +0200

    Challenge 3 finished

--- 02-Fundamentals-Part-2/starter/Challenges/codeChallenge1.js
+++ 02-Fundamentals-Part-2/starter/Challenges/codeChallenge1.js
@@ -1,5 +1,5 @@
 /* Challenge Mark y Jhon.
-comparan BMI.*/
+comparan BMI.
 
 const markMass = 78;
 const markHeight = 1.69;
@@ -18,4 +18,48 @@ if (bmiMark > bmiJhon) {
   console.log(`Mark's BMI(${bmiMark}) is higher than Jhon's BMI(${bmiJhon})!`);
 } else {
   console.log(`Jhon's BMI(${bmiJhon}) is higher than Mark's BMI(${bmiMark}) !`);
+};*/
+
+
+// 1. Crea un objeto para cada uno con las propiedades para su nombre completo, etc.
+// 2. Crea una funcion dentro del objeto para calcular el BMI, guarda el valor del BMI
+// en una propiedad y también retornala de la funcion.
+// 3. Log en la console quien tiene el bMI mas alto, junto con el nombre completo y el BMI.
+
+
+
+
+const mark = {
+  firstName: 'Mark',
+  lastName: 'Miller',
+  mass: 78,
+  height: 1.69,
+
+  calcBMI: function () {
+    this.totalBMI = this.mass / this.height ** 2;
+    return this.totalBMI;
+  }
 };
+
+const john = {
+  firstName: 'Jhon',
+  lastName: 'Smith',
+  mass: 92,
+  height: 1.95,
+
+  calcBMI: function () {
+    this.totalBMI = this.mass / this.height ** 2;
+    return this.totalBMI;
+  }
+};
+
+mark.calcBMI();
+john.calcBMI();
+
+if (mark.totalBMI > john.totalBMI) {
+  console.log (`${mark.firstName} ${mark.lastName} BMI(${mark.totalBMI}) is higher than ${john.firstName} ${john.lastName} BMI(${john.totalBMI})`);
+} else {
+  console.log (`${john.firstName} ${john.lastName} BMI(${john.totalBMI}) is higher than ${mark.firstName} ${mark.lastName} BMI(${mark.totalBMI})`);
+};
+
+
