85c22213489657295a24a53fd8bb46b5b8d36793
Eric Sández <esandez93@gmail.com>
2021-07-07 17:50:20 +0200

    First commit

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,65 @@
+const jonas = {
+  firstName: 'Jonas',
+  lastName: 'Schmedtmann',
+  birthYear: 2006,
+  job: 'teacher',
+  friends: ['Michael', 'Peter', 'Steven'],
+
+  calcAge: function() {
+    this.age = 2020 - this.birthYear;
+    return this.age;
+  },
+
+  driversLicense: function () {
+    /* if (this.age >= 18) {
+      return 'a';
+    } else {
+      return 'no';
+    } */
+    if (this.age >= 18) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+};
+
+ // calcAge: function(birthYear) {
+ //   return 2020 - birthYear;
+ // }
+
+//  calcAge: function() {
+//    //console.log(this);
+//    return 2020 - this.birthYear;
+//  }
+
+// calcAge: function() {
+//   this.age = 2020 - birthYear;
+// }
+// };
+
+// console.log(jonas.calcAge());
+
+// console.log(jonas.calcAge());
+// console.log(jonas.calcAge());
+// console.log(jonas.calcAge());
+
+//CHALLENGE
+// "Jonas is a 29-year old teacher, and he has a/no driver's license."
+// `${jonas.firstname} is a ${jonas.calcAge()}-year old ${jonas.job}, and he has ${jonas.driversLicense} driver's license.`
+// Tengo que hacer la frase que hay arriba utilizando el objeto jonas.
+// -
+
+console.log(jonas.calcAge());
+console.log(`${jonas.firstName} is a ${jonas.calcAge()}-year old ${jonas.job}, and he has ${jonas.driversLicense() ? 'a' : 'no'} driver's license.`);
+
+console.log(jonas.driversLicense);
+console.log(jonas.driversLicense());
+
+if (jonas.driversLicense) {
+  // return 'a';
+} else {
+  // return 'no';
+}
+
+
