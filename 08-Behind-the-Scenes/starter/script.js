aeaf0e8069cab647ebaa9774b9e1458eab625e53
Sara Poblador Pérez <sarapbldr@icloud.com>
2021-08-17 15:04:26 +0200

    BTS JS practice

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,31 @@
 'use strict';
+
+function calcAge(birthYear) {
+  const age = 2021 - birthYear;
+
+  function printAge() {
+    let output = `${firstName} you are  ${age}, born in ${birthYear}`;
+    console.log(output);
+
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      var millenial = true;
+
+      const firstName = 'Steven';
+      const str = `Oh, and you're a millenial, ${firstName}`;
+      console.log(str);
+
+      function add(a, b) {
+        return a + b;
+      }
+
+      output = 'NEW OUTPUT!';
+    }
+    console.log(millenial);
+    console.log(output);
+  }
+  printAge();
+  return age;
+}
+
+const firstName = 'Jonas';
+calcAge(1991);
