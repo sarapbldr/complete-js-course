c78dbc1dae2410fd3c4e681183eb6e4fcf9c2026
Sara Poblador <sarapbldr@MBP-de-Sara.home>
2021-08-19 19:47:13 +0200

    Primitives vs Objects practice

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -114,7 +114,7 @@ var addArrow = (a, b) => {
 };
 
 addArrow(2, 5, 8);
-*/
+
 
 let age = 30;
 let oldAge = age;
@@ -130,3 +130,38 @@ const friend = me;
 friend.age = 28;
 console.log('Friend:', friend);
 console.log('Me:', me);
+*/
+
+// Primitive types
+let lastName = 'Williams';
+let oldLastName = lastName;
+lastName = 'Davis';
+console.log(lastName);
+console.log(oldLastName);
+
+// Reference types
+const jessica = {
+  firstName: 'Jessica',
+  lastName: 'Williams',
+  age: 27,
+};
+const marriedJessica = jessica;
+marriedJessica.lastName = 'Davis';
+console.log('Before marriage:', jessica);
+console.log('After marriage:', marriedJessica);
+
+// Copying objects
+const jessica2 = {
+  firstName: 'Jessica',
+  lastName: 'Williams',
+  age: 27,
+  family: ['Alice', 'Bob'],
+};
+
+const jessicaCopy = Object.assign({}, jessica2);
+jessicaCopy.lastName = 'Davis';
+
+jessicaCopy.family.push('Mary', 'John');
+
+console.log('Before marriage:', jessica2);
+console.log('After marriage:', jessicaCopy);
